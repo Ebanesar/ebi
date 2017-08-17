1f9981cc8a555256da7c786899e5d775760c4ae3
kaarthikraaj <kaarthikraaj@palmtreeinfotech.com>
2017-08-17 17:21:34 +0530

    Added missed out file

---
+++ image/src/main/java/com/palmtree/content/analysis/image/model/ImageSafetyResponse.java
@@ -0,0 +1,16 @@
+package com.palmtree.content.analysis.image.model;
+
+/**
+ * Created by kaarthikraaj on 17/8/17.
+ */
+public class ImageSafetyResponse {
+    boolean isSafe;
+
+    public boolean isSafe() {
+        return isSafe;
+    }
+
+    public void setSafe(boolean safe) {
+        isSafe = safe;
+    }
+}
