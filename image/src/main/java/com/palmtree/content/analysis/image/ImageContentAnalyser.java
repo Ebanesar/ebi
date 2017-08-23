2d1096d365d83da872d4aac898f23d9207a204e0
ebi <ebanesarc@karunya.edu.in>
2017-08-23 14:36:46 +0530

    changed

--- image/src/main/java/com/palmtree/content/analysis/image/ImageContentAnalyser.java
+++ image/src/main/java/com/palmtree/content/analysis/image/ImageContentAnalyser.java
@@ -1,9 +1,12 @@
 package com.palmtree.content.analysis.image;
 import com.google.cloud.vision.v1.*;
 import com.google.cloud.vision.v1.Feature.Type;
+import com.google.common.collect.Iterators;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 /**
  * Created by kaarthikraaj on 10/8/17.
@@ -42,12 +45,11 @@ public class ImageContentAnalyser {
             SafeSearchAnnotation safeAnnotation =  annotateImageResponse.getSafeSearchAnnotation();
             System.out.println("Violence value is "+safeAnnotation.getViolence().getNumber());
             System.out.println("Adult Content value is "+safeAnnotation.getAdultValue());
-
+            WebDetection wb3 = annotateImageResponse.getWebDetection();
+            List<WebDetection.WebEntity> wb4 = wb3.getWebEntitiesList();
             List<EntityAnnotation> labelAnnotations =  annotateImageResponse.getLabelAnnotationsList();
             for (EntityAnnotation entityAnnotation:labelAnnotations)
             {
-                  WebDetection wb3 = annotateImageResponse.getWebDetection();
-                  List<WebDetection.WebEntity> wb4 = wb3.getWebEntitiesList();
                   for (WebDetection.WebEntity wb5 : wb3.getWebEntitiesList())
                   {
                      if (safeAnnotation.getViolence().getNumber()>1
@@ -80,6 +82,12 @@ public class ImageContentAnalyser {
 
     public boolean isValidImage(String imageURI){
         boolean isValid = true;
+        int a=0;
+        ArrayList<String> list = new ArrayList<String>(); // Creating the array list
+        list.add("head");
+        list.add("Man");
+        Iterator<String> iterators = list.iterator();
+
         try {
             ImageAnnotatorClient visionClient =  ImageAnnotatorClient.create();
             ArrayList<AnnotateImageRequest> imageReqsList = new ArrayList<AnnotateImageRequest>();
@@ -95,8 +103,14 @@ public class ImageContentAnalyser {
             BatchAnnotateImagesResponse response = visionClient.batchAnnotateImages(imageReqsList);
                //System.out.println(response.getResponsesCount());
             List<AnnotateImageResponse> annotateImageResponses = response.getResponsesList();
+
+
             for(AnnotateImageResponse annotateImageResponse : annotateImageResponses)
             {
+                List<EntityAnnotation> labelAnnotations =  annotateImageResponse.getLabelAnnotationsList();
+                WebDetection wb = annotateImageResponse.getWebDetection();
+                List<WebDetection.WebEntity> wb1 = wb.getWebEntitiesList();
+               // List<FaceAnnotation> fa = annotateImageResponse.getFaceAnnotationsList();
                //System.out.println(annotateImageResponse.getLogoAnnotationsCount());
                //System.out.println(annotateImageResponse.getTextAnnotationsCount());
                //System.out.println(annotateImageResponse.());
@@ -104,38 +118,108 @@ public class ImageContentAnalyser {
                 boolean hasError = annotateImageResponse.hasError();
                 System.out.println(annotateImageResponse.getError().getMessage());
 
-            List<EntityAnnotation> labelAnnotations =  annotateImageResponse.getLabelAnnotationsList();
-            for (EntityAnnotation entityAnnotation:labelAnnotations)
+            for (WebDetection.WebEntity wb2 : wb.getWebEntitiesList())
             {
-                 WebDetection wb = annotateImageResponse.getWebDetection();
-                 List<WebDetection.WebEntity> wb1 = wb.getWebEntitiesList();
-                 for (WebDetection.WebEntity wb2 : wb.getWebEntitiesList())
-                 {    //if (wb2.getDescription().equalsIgnoreCase("Tattoo"))
-                     //System.out.println( faceAnnotation.getHeadwearLikelihoodValue());
-                     //EntityAnnotation entityAnnotation = annotateImageResponse.getLabelAnnotations(0);
-                    if(entityAnnotation.getDescription().equalsIgnoreCase("sunglasses")
-                      || entityAnnotation.getDescription().equalsIgnoreCase("Headgear")
-                      || entityAnnotation.getDescription().equalsIgnoreCase("Mask")
-                      || entityAnnotation.getDescription().equalsIgnoreCase("Helmet")
-                      || entityAnnotation.getDescription().equalsIgnoreCase("Turban")
-                      || entityAnnotation.getDescription().equalsIgnoreCase("Fedora")
-                      || wb2.getDescription().equalsIgnoreCase("Tattoo")
-                      || wb2.getDescription().equalsIgnoreCase("Mask")
-                      || wb2.getDescription().equalsIgnoreCase("Headwear")
-                      || wb2.getDescription().equalsIgnoreCase("Headscarf"))
+            for(EntityAnnotation entityAnnotation:labelAnnotations)   //Face Focus
+            {
+                if (entityAnnotation.getDescription().contains("chin")
+                        || entityAnnotation.getDescription().contains("Nose")
+                        || entityAnnotation.getDescription().contains("Forehead")
+                        || entityAnnotation.getDescription().contains("Mouth")
+                        || entityAnnotation.getDescription().contains("Neck")
+                        || entityAnnotation.getDescription().contains("cheek")
+                        || entityAnnotation.getDescription().contains("jaw")
+                        || entityAnnotation.getDescription().contains("ear")
+                        || entityAnnotation.getDescription().contains("smile")
+                        || entityAnnotation.getDescription().contains("Eyebrow")
+                        || entityAnnotation.getDescription().contains("head"))
+                {
+                    a=1;
+                }
 
-                    return false;
-                 }
+                else if (entityAnnotation.getDescription().contains("jeans")
+                        || entityAnnotation.getDescription().contains("Standing")
+                        || entityAnnotation.getDescription().contains("Trouser")
+                        || entityAnnotation.getDescription().contains("Shoe")
+                        || entityAnnotation.getDescription().contains("Gentleman")
+                        || entityAnnotation.getDescription().contains("Suit")
+                        || entityAnnotation.getDescription().contains("Formal Wear")
+                        || entityAnnotation.getDescription().contains("T Shirt"))   //Full image
+                {
+                     a=2;
+                }
+
+
+
+         switch (a)
+         {
+             case 1:
+             {
+              if (entityAnnotation.getDescription().equalsIgnoreCase("sunglasses")
+                        || entityAnnotation.getDescription().equalsIgnoreCase("Headgear")
+                        || entityAnnotation.getDescription().equalsIgnoreCase("Mask")
+                        || entityAnnotation.getDescription().equalsIgnoreCase("Helmet")
+                        || entityAnnotation.getDescription().equalsIgnoreCase("Turban")
+                        || entityAnnotation.getDescription().equalsIgnoreCase("Fedora")
+                        || entityAnnotation.getDescription().equalsIgnoreCase("Mammal")
+                        || entityAnnotation.getDescription().equalsIgnoreCase("Animal")
+                        || entityAnnotation.getDescription().equalsIgnoreCase("Hand")
+                        || entityAnnotation.getDescription().equalsIgnoreCase("Tattoo")
+                        || wb2.getDescription().equalsIgnoreCase("Silhouette")
+                        || wb2.getDescription().equalsIgnoreCase("Tattoo")
+                        || wb2.getDescription().equalsIgnoreCase("Mask")
+                        || wb2.getDescription().equalsIgnoreCase("Headwear")
+                        || wb2.getDescription().equalsIgnoreCase("Headscarf")
+                        || wb2.getDescription().equalsIgnoreCase("sunglasses")
+                        || wb2.getDescription().equalsIgnoreCase("Animal")
+                        || wb2.getDescription().equalsIgnoreCase("Mammal"))
+                           {
+                                isValid = false;
+                           }
+                                    break;
+
+
+             }
+             case 2:
+             {
+                if (entityAnnotation.getDescription().equalsIgnoreCase("sunglasses")
+                             || entityAnnotation.getDescription().equalsIgnoreCase("Headgear")
+                             || entityAnnotation.getDescription().equalsIgnoreCase("Mask")
+                             || entityAnnotation.getDescription().equalsIgnoreCase("Helmet")
+                             || entityAnnotation.getDescription().equalsIgnoreCase("Turban")
+                             || entityAnnotation.getDescription().equalsIgnoreCase("Fedora")
+                             || entityAnnotation.getDescription().equalsIgnoreCase("Mammal")
+                             || entityAnnotation.getDescription().equalsIgnoreCase("Animal")
+                             || entityAnnotation.getDescription().equalsIgnoreCase("Tattoo")
+                             || wb2.getDescription().equalsIgnoreCase("Silhouette")
+                             || wb2.getDescription().equalsIgnoreCase("Tattoo")
+                             || wb2.getDescription().equalsIgnoreCase("Mask")
+                             || wb2.getDescription().equalsIgnoreCase("Headwear")
+                             || wb2.getDescription().equalsIgnoreCase("Headscarf")
+                             || wb2.getDescription().equalsIgnoreCase("sunglasses")
+                             || wb2.getDescription().equalsIgnoreCase("Animal")
+                             || wb2.getDescription().equalsIgnoreCase("Mammal"))
+                     {
+                         isValid = false;
+                     }
+                     break;
+
+             }
+
+         }
+
             }
-            }
-                // p.getName()
+
+        }
+    }
+                             // p.getName()
         }
         catch (IOException e)
         {
             e.printStackTrace();
         }
         //Invoke Cloud Vision API and based on the response return if its valid or not.
-        return isValid;
+      return isValid;
     }
 
     public static  void main(String args[])
@@ -143,21 +227,23 @@ public class ImageContentAnalyser {
         ImageContentAnalyser analyser = new ImageContentAnalyser();
         System.out.println(System.getenv("GOOGLE_APPLICATION_CREDENTIALS"));
         //System.out.println(analyser.isImageSafe("https://cdn.pornpics.com/pics1/2016-07-15/361749_12big.jpg"));
-        if (analyser.isValidImage("https://ae01.alicdn.com/kf/HTB1zy1TLpXXXXb9XVXXq6xXFXXXp/Fashion-Bufanda-Tubular-Hijab-Camo-Bandana-font-b-Scarf-b-font-Seamless-Neck-Tube-Bandana-Standard.jpg")==false)
-         {
-            System.out.println("InValid Image");
-         }
-         else
-         {
-            System.out.println("Valid Image");
-         }
-      /* if (analyser.isImageSafe("https://cdn.munplanet.com/storage/uploads/52209627db7c13603b000001/topic/background_image/54302218db7c1366d600b544/o-TERRORISM-facebook.jpg")==false)
+       if (analyser.isValidImage("http://www.dhresource.com/albu_1112953851_00-1.0x0/wholesale-new-fashion-korean-brand-gentle.jpg")==false)
+        {
+            System.out.println("Invalid Image");
+        }
+      else
+        {
+           System.out.println("Valid Image");
+        }
+
+   /*    if (analyser.isImageSafe("https://image.shutterstock.com/z/stock-photo-bald-man-face-closed-hands-279396935.jpg")==false)
         {
             System.out.println("Unsafe Image");
         }
         else
         {
             System.out.println("Safe Image");
-        } */
+        }       */
+
     }
- }
+}
