393770c13b5cb6f0ce77ee3f0a722f448478c10c
ebi <ebanesarc@karunya.edu.in>
2017-10-10 15:54:50 +0530

    got output

--- image/src/main/java/com/palmtree/content/analysis/image/ImageContentAnalyser.java
+++ image/src/main/java/com/palmtree/content/analysis/image/ImageContentAnalyser.java
@@ -28,10 +28,11 @@ public class ImageContentAnalyser {
         // System.out.println(analyser.detectTexts("http://www.gsproducts.co.uk/wordpress/wp-content/uploads/2015/04/Boat-name-Mariah.jpg"));
 
         HashMap<String,String> input_hashmap = new HashMap<String, String>();
-        input_hashmap.put("Name","Gonzalo LLorens Chanza");
-        input_hashmap.put("Nationality","Spanish");
+        input_hashmap.put("Name","Ebanesar");
+        input_hashmap.put("GENDER","Male");
         String docType = null;
-        analyser.generateTemplate("http://www.bongdaao.com/wp-content/uploads/2017/10/professional-resume-format-samples-free-download-unique-free-cv-europass-pdf-europass-home-european-cv-format-pdf-of-professional-resume-format-samples-free-download.png", input_hashmap, docType);
+        analyser.generateTemplate("https://00e9e64bac6ee59dd9d5bfe291832270f2d8ac4ff822079bcf-apidata.googleusercontent.com/download/storage/v1/b/staging.palmtree-image-01.appspot.com/o/id-1919331104995787.jpg?qk=AD5uMEu-VJl5uoZWm6PD9-4ZOKxXw-eAJsE2TVjuPiRNze1FyT0iqBI2y1i2BywzLo0-_3spSSz1ysFyB_sBK6b02dy-AqMd5_P30GJ3c8ckmo0SRMHbTBZWzBb-XVrTPVBNqxzd662pZY-ip0ljaJPdGx5JaqKjxLcoPzPqB7vxx6NWsLGi6PiGyT9veIJKD8sTgm6YJhkFr6tFhbFqawJp11tEGN_vvk13CavKmd43WStf_1vGUcoJ5QWvcvWJJ8ljfQQu2WHoilmiTQUzGyIwXNCsIYYhjc6YClem9GUfmKyaUiGZvotrEvRRFWHmvuoCh5DQfkTD1PrgGtfTEHkwCOq59GTRZGEc5bY4s_ogiECmXpTO-NMSHaZ7AgGB--mpN_6zZ9CchOa7I93SBmraIlJip1hQl4Shxjd3yPi8rSQswwULKhyttl72yRrdOt8tbnIIhUplnNw-q0NuTf85vI2DJ6IugNyeb1Q2Qaz1HLoDur3RRl7nSSe-K249QYnTNoWUwf6ZN10O-wH7KDkq11XEcSOn4xvdnNTCs3f394wIa3uogaUNI6_Ub_jBx_X6UOKdjah231mi1pqS_MeuczZcK4CpCifTBk64jvzcTm-lWvFupeG_10I6C-M0mhKH3qkP-LNwrjo-S9uCnTQjoy7-8Ed7wy0QF-rPpJbxoZn-lQg_5Pqq0Fep2J3t0hzlJyvJxCOI0LG8Vx5Yy59X7PhEtro4Xnk6VoJJnPJMq4jTT06vzjAQmNFpz-mzRYlyl4GtdOeAF5J7pv_yETxGMrVE4-XcOg", input_hashmap, docType);
+      System.out.println(analyser.extractFieldValueUsingTemplate("https://00e9e64bac21237c842d75ca29eba29d20429f112df766bd55-apidata.googleusercontent.com/download/storage/v1/b/staging.palmtree-image-01.appspot.com/o/id-804450479735093.jpg?qk=AD5uMEs6UocotM0ZqD-n99lMzuJSszpA4iHET3WuIxSxMjcepRLkVzXi9UTbgKRgskf_PiNkVOPa_O3UVBfI90NYS3FbAA9u34pWC7dWfVjFX2HxJDrmmtwH04eyPEyBjalJdCt88e5pG-qp4WQfv_OopYHNciKzFgcIbkOXHM1YPpcJnnGITJ4pU3zEWsx5sm_KrjsbWqSpdQm1DiOttM8kB-5fkNVmNGc8x21x-exnlL6G0COD6Rm2USc9itBPtwnELA-lyiXIw0rrHGRPoZoQeDq-ibhYdXadRFtWhnRe3rNYDb2ilsbOD2bN_LJh-C5_vBc0zfCdzPdqq1LzZtPa1_64comiB2w-grVRCIhT3UwUgqu6oYiPhAzgD1UxeWGrkcMQT3vc-indWfeb2P_TcCUSqYZSru31av1q9y_7wm8GWVJr0FpJbOxHBBPa68x4CBjMCtiY5KmzaM3Diaj7DfWLX4X4ZSIpVRVWv4iIRgs-Vaa7Z3-wsP2lAhmV1Ao1xrrFjLE6env3dCKRjkk1xLlBgtY1KFcr_YAwy4XDp_3LT0zNP1ZsEO5Y9cbu_U-YIfksdEDRgvWXYm6Ym6z6eqdS9xXojnL9USOMIzX0XhMuasT7vmgmmz_YyTf45NmP9pT7ubbV28DdfWzMWDvkAruzggu-Nbmndd-8n9WbOwqtUa7IzyOoUn5wCb50W6X8kExnDyczMO-hZ3VHXvih654bAOXz6D29cYx0i_OZp_oYwxCFxPAd8ZjhaYnSHVV0DAjFVAnZgFCUwG13r7C3m8jfB1TdeQ" , docType));
     }
 
     HashMap<String,TextFieldArea> template = new HashMap<String, TextFieldArea>();
@@ -48,10 +49,10 @@ public class ImageContentAnalyser {
             text_positionhashmap = extractTextsAndLocation(imageURI);
             textFieldArea= getPostionOfValueForLabel(valueForLabel, text_positionhashmap);
             template.put(labels,textFieldArea);
-            outputHashMap = extractFieldValueUsingTemplate(imageURI,docType);
+          //  outputHashMap = extractFieldValueUsingTemplate(imageURI,docType);
         }
-        System.out.println(outputHashMap);
-        templateRegistry.put(docType,template);
+       // System.out.println(outputHashMap);
+       // templateRegistry.put(docType,template);
         return true;
         }
 
@@ -61,15 +62,16 @@ public class ImageContentAnalyser {
 
         Set<String> labelValue = template.keySet();
         HashMap <String,TextFieldArea> text_positionhashmap = new HashMap<String,TextFieldArea>();
-        HashMap<String,String> outputHashmap = new HashMap<String, String>();
+        HashMap <String,String> outputHashmap = new HashMap<String, String>();
         text_positionhashmap = extractTextsAndLocation(imageURI);
 
-            for (String value:labelValue)
+        for (String value:labelValue)
         {
             TextFieldArea textFieldArea_bigRect =  template.get(value);
-            String value_text = extractTextValueForLabel(textFieldArea_bigRect,text_positionhashmap);
-            outputHashmap.put(value,value_text);
+           String value_text = extractTextValueForLabel(textFieldArea_bigRect,text_positionhashmap);
+           outputHashmap.put(value,value_text);
         }
+
         return outputHashmap;
     }
 
@@ -195,6 +197,68 @@ public class ImageContentAnalyser {
         }
         text = builder.toString();
         return text;
+    }
+
+
+
+
+
+
+
+
+
+    public boolean tryingRearrange (String imageURI) {
+        try {
+            ImageAnnotatorClient visionClient = ImageAnnotatorClient.create();
+            ArrayList<AnnotateImageRequest> imageReqsList = new ArrayList<AnnotateImageRequest>();
+            Image image = Image.newBuilder().setSource(ImageSource.newBuilder().setImageUri(imageURI)).build();
+            AnnotateImageRequest imageReq = AnnotateImageRequest.newBuilder().setImage(image)
+                    .addFeatures(Feature.newBuilder().setType(Type.LABEL_DETECTION).build())
+                    .addFeatures(Feature.newBuilder().setType(Type.TEXT_DETECTION).build())
+                    .build();
+            imageReqsList.add(imageReq);
+            BatchAnnotateImagesResponse response = visionClient.batchAnnotateImages(imageReqsList);
+            List<AnnotateImageResponse> annotateImageResponses = response.getResponsesList();
+            List<String> Description = new ArrayList<String>();
+            List<List<Vertex>> vertex = new ArrayList<List<Vertex>>();
+            List<Vertex> polygon = new ArrayList<Vertex>();
+            String text = "";
+            float left_Top_X_Pos = 0;                   float right_Top_X_Pos = 0;
+            float left_Bottom_X_Pos = 0;                float right_Bottom_X_Pos = 0;
+            float left_Top_Y_Pos = 0;                   float right_Top_Y_Pos = 0;
+            float left_Bottom_Y_Pos = 0;                float right_Bottom_Y_Pos = 0;
+
+
+            for (AnnotateImageResponse annotateImageResponse : annotateImageResponses) {
+                for (EntityAnnotation poly : annotateImageResponse.getTextAnnotationsList()) {
+                    Description.add(poly.getDescription());
+                    vertex.add(poly.getBoundingPoly().getVerticesList());   }  }
+
+            for (int i = 1; i < vertex.size(); i++) {
+                text = Description.get(i);
+                polygon = vertex.get(i);
+
+                Vertex First_co_ordinate = polygon.get(0);
+                Vertex Second_co_ordinate = polygon.get(1);
+                Vertex Third_co_ordinate = polygon.get(2);
+                Vertex Forth_co_ordinate = polygon.get(3);
+
+                left_Top_X_Pos = First_co_ordinate.getX();
+                left_Top_Y_Pos = First_co_ordinate.getY();
+                right_Top_X_Pos = Second_co_ordinate.getX();
+                right_Top_Y_Pos = Second_co_ordinate.getY();
+                right_Bottom_X_Pos = Third_co_ordinate.getX();
+                right_Bottom_Y_Pos = Third_co_ordinate.getY();
+                left_Bottom_X_Pos = Forth_co_ordinate.getX();
+                left_Bottom_Y_Pos = Forth_co_ordinate.getY();
+                TextFieldArea textFieldArea = new TextFieldArea(left_Bottom_X_Pos, left_Bottom_Y_Pos,
+                        right_Bottom_X_Pos, right_Bottom_Y_Pos, right_Top_X_Pos, right_Top_Y_Pos, left_Top_X_Pos, left_Top_Y_Pos);
+
+            }
+        } catch (IOException exc) {
+            logger.error("Exception while reading image from the url" + exc.getMessage());
+        }
+        return true;
     }
 
 
@@ -407,8 +471,6 @@ public class ImageContentAnalyser {
         }
         return logosDetected;
     }
-
-
 }
 
 
